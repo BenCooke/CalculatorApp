17ee2817a522e0b73db55dba3397301e53caba7e
BenCooke <ben.thomas.cooke@gmail.com>
2018-02-05 16:27:29 +0000

    Add files via upload
    functionality added for all numbers, plus sign and equals sign

--- BenCalculator/BenCalculator/ViewController.swift
+++ BenCalculator/BenCalculator/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var firstNumber:String = ""
+    var symbol:String = ""
     
     
     
-    
-    
-    
-    
+    @IBOutlet weak var numberField: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -28,6 +28,80 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    @IBAction func clear(_ sender: Any) {
+        numberField.text = ""
+    }
+    
+    @IBAction func zeroButton(_ sender: Any) {
+        numberField.text =  numberField.text! + "0"
+    }
+    
+    
+    @IBAction func oneButton(_ sender: Any) {
+        numberField.text = numberField.text! + "1"
+    }
+    
+    
+    @IBAction func twoButton(_ sender: Any) {
+        numberField.text = numberField.text! + "2"
+    }
+    
+    @IBAction func threeButton(_ sender: Any) {
+        numberField.text = numberField.text! + "3"
+    }
+    
+    
+    @IBAction func fourButton(_ sender: Any) {
+        numberField.text = numberField.text! + "4"
+    }
+    
+    @IBAction func fiveButton(_ sender: Any) {
+        numberField.text = numberField.text! + "5"
+    }
+    
+    
+    @IBAction func sixButton(_ sender: Any) {
+        numberField.text = numberField.text! + "6"
+    }
+    
+    
+    @IBAction func sevenButton(_ sender: Any) {
+        numberField.text = numberField.text! + "7"
+    }
+    
+    
+    @IBAction func eightButton(_ sender: Any) {
+        numberField.text = numberField.text! + "8"
+    }
+    
+    
+    @IBAction func nineButton(_ sender: Any) {
+        numberField.text = numberField.text! + "9"
+    }
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func plus(_ sender: Any) {
+        firstNumber = numberField.text!
+        numberField.text = ""
+        symbol = "+"
+        
+    }
 
+    @IBAction func equals(_ sender: UIButton) {
+        if (symbol=="+"){
+            let answer = Int(firstNumber)! + Int(numberField.text!)!
+            numberField.text = String(answer)
+        }
+        
+        
+        
+    }
 }
 
